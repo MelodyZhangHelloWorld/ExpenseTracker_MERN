0bcfc8dcd9d1688c8fef031f0c3a948501fa8a58
MelodyZhangHelloWorld <ge.zhang01@edu.sait.ca>
2020-12-09 18:16:02 -0700

    build for production
    Signed-off-by: MelodyZhangHelloWorld <ge.zhang01@edu.sait.ca>

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');  //* after run build
 const express = require('express');
 const dotenv = require('dotenv'); // allows to create global varialbes
 const colors = require('colors'); //extra
@@ -24,6 +25,14 @@ if(process.env.NODE_ENV === 'development'){
 // app.get('/', (req, res) => res.send('<h1>Test out the backend!</h1>'));
 //mount the route:
 app.use('/api/v1/transactions', transactions);
+
+//BELOW the api routes
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => 
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
